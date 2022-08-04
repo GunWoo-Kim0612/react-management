f1904868dd4e6adecdbcdb1be573c7621c9df8e0
GunWoo-Kim0612 <gwk0624333@gmail.com>
2022-08-04 11:47:41 +0900

    create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//함수형태 -> 클래스형태
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+const customer = {
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',      //dummy image 
+  'name': '홍길동',
+  'birthday': '20220101',
+  'gender': '남자',
+  'job': '대학생',
+
 }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>
+          <Customer
+            id={customer.id}
+            image={customer.image}
+            name={customer.name}
+            birthday={customer.birthday}
+            gender={customer.gender}
+            job={customer.job}
+          ></Customer>
+        </p>
+      </div>
+    );
+  }
+}
+
+
 
 export default App;
