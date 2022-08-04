8b0705c2778b5dc5f1f8f77efdc1736abfa9376d
GunWoo-Kim0612 <gwk0624333@gmail.com>
2022-08-04 11:53:45 +0900

    modify Cusomer to Customers array

--- src/App.js
+++ src/App.js
@@ -26,30 +26,62 @@ import Customer from './components/Customer';
 //   );
 // }
 
-const customer = {
-  'id': 1,
-  'image': 'https://placeimg.com/64/64/1',      //dummy image 
-  'name': '홍길동',
-  'birthday': '20220101',
-  'gender': '남자',
-  'job': '대학생',
-
-}
+const customers = [
+  {
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',      //dummy image site 64/64 사이즈
+    'name': '홍길동',
+    'birthday': '20220101',
+    'gender': '남자',
+    'job': '대학생',
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',      //dummy image site 64/64 사이즈
+    'name': '김길동',
+    'birthday': '20220102',
+    'gender': '남자',
+    'job': '개발자',
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',      //dummy image site 64/64 사이즈
+    'name': '이길동',
+    'birthday': '20220103',
+    'gender': '여자',
+    'job': '소프트웨어 엔지니어',
+  }
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>
-          <Customer
-            id={customer.id}
-            image={customer.image}
-            name={customer.name}
-            birthday={customer.birthday}
-            gender={customer.gender}
-            job={customer.job}
-          ></Customer>
-        </p>
+        {/* ctrl + d로 인덱스 multiple 선택 가능 */}
+        <Customer
+          id={customers[0].id}
+          image={customers[0].image}
+          name={customers[0].name}
+          birthday={customers[0].birthday}
+          gender={customers[0].gender}
+          job={customers[0].job}
+        ></Customer>
+        <Customer
+          id={customers[1].id}
+          image={customers[1].image}
+          name={customers[1].name}
+          birthday={customers[1].birthday}
+          gender={customers[1].gender}
+          job={customers[1].job}
+        ></Customer>
+        <Customer
+          id={customers[2].id}
+          image={customers[2].image}
+          name={customers[2].name}
+          birthday={customers[2].birthday}
+          gender={customers[2].gender}
+          job={customers[2].job}
+        ></Customer>
       </div>
     );
   }
